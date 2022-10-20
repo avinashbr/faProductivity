44531d4e826bb30ff0cc51bf31f2f9a7c9e6df48
avinash br <avinash.shidle@gmail.com>
2022-10-20 19:35:29 +0530

    first commit

---
+++ src/test/java/com/faProductivity/faProductivity/FaProductivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faProductivity.faProductivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaProductivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
