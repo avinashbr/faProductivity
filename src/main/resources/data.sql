dffd483fc386895fc27047b71fbcfa8e2d3b0034
avinash br <avinash.shidle@gmail.com>
2022-10-21 16:28:33 +0530

    some more template added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 CREATE TABLE Command_Template (id int, template_Name varchar(255),regex varchar(255));
 INSERT INTO Command_Template (id, template_Name,regex) VALUES (1, 'Open customerName profile in client management','Open profile in client management');
 INSERT INTO Command_Template (id, template_Name,regex) VALUES (2, 'what is the balance of customerName savings account','what is the balance of savings account');
+INSERT INTO Command_Template (id, template_Name,regex) VALUES (3, 'Transfer money from account1 to account2','Transfer from to ');
+INSERT INTO Command_Template (id, template_Name,regex) VALUES (4, 'Check account balance in account account1','Check account balance in account ');
+INSERT INTO Command_Template (id, template_Name,regex) VALUES (5, 'Open Allen Smith profile in client management','Open Allen in client management');
